aef10932b2493585c7d0bc9a0f32b74cdb4dc602
= <stephen.carlylesmith@googlemail.com>
2022-04-24 12:19:44 +0100

    Loads Assassins map

--- src/Map.gd
+++ src/Map.gd
@@ -4,6 +4,73 @@ func _ready():
 	var astar : AStar2D = AStar2D.new()
 
 	var csn_class = preload("res://Wall.tscn")
+	var CITY_SIZE = 48 # todo - get from file
+
+	var bmap = {} # Vector2, bool=floor
+	var nodes = {} # Vector2, id 
+
+	var file : File = File.new()
+	var res = file.open("res://Data/theassassins.csv", file.READ)
+	if res != 0:
+		push_error("Error opening map file")
+	
+	file.get_csv_line() # Skip header
+	var y: int = -1
+	while !file.eof_reached():
+		y += 1
+		var csv : PoolStringArray = file.get_csv_line()
+		if csv.size() <= 2:
+			continue
+		for x in CITY_SIZE:
+			var xpos = x * Globals.SQ_SIZE
+			var ypos = y * Globals.SQ_SIZE
+			var vec = Vector2(xpos, ypos)
+			if csv[x].begins_with("FLOOR") == false:
+				bmap[vec] = false
+				# Wall
+				var wall = csn_class.instance()
+				wall.position.x = xpos
+				wall.position.y = ypos
+				self.add_child(wall)
+			else:
+				# Floor
+				var id = astar.get_available_point_id()
+				bmap[vec] = true
+				astar.add_point(id, vec)
+				nodes[vec] = id
+	file.close()
+				
+	# Connect nodes
+	for y2 in CITY_SIZE:
+		for x in CITY_SIZE:
+			y = y2 # todo - remove this
+			if x == 0 or y == 0 or x >= CITY_SIZE-2 or y >= CITY_SIZE-2: # Avoid OOB edges
+				continue
+			var xpos = x * Globals.SQ_SIZE
+			var ypos = y * Globals.SQ_SIZE
+			var vec = Vector2(xpos, ypos)
+			if bmap[vec] == true: # floor
+				var vec_l = Vector2(xpos-Globals.SQ_SIZE, ypos)
+				if bmap[vec_l]:
+					astar.connect_points(nodes[vec], nodes[vec_l])
+				var vec_r = Vector2(xpos+Globals.SQ_SIZE, ypos)
+				if bmap[vec_r]:
+					astar.connect_points(nodes[vec], nodes[vec_r])
+				var vec_u = Vector2(xpos, ypos-Globals.SQ_SIZE)
+				if bmap[vec_u]:
+					astar.connect_points(nodes[vec], nodes[vec_u])
+				var vec_d = Vector2(xpos, ypos+Globals.SQ_SIZE)
+				if bmap[vec_d]:
+					astar.connect_points(nodes[vec], nodes[vec_d])
+					
+
+	Globals.astar = astar
+	pass
+
+func _ready_ORIGINAL():
+	var astar : AStar2D = AStar2D.new()
+
+	var csn_class = preload("res://Wall.tscn")
 	var CITY_SIZE = Globals.MAP_SIZE
 
 	var bmap = {} # Vector2, bool=floor
@@ -32,7 +99,7 @@ func _ready():
 	# Connect nodes
 	for y in CITY_SIZE:
 		for x in CITY_SIZE:
-			if x == 0 or y == 0 or x == CITY_SIZE-2 or y == CITY_SIZE-2:
+			if x == 0 or y == 0 or x == CITY_SIZE-2 or y == CITY_SIZE-2: # Avoid OOB edges
 				continue
 			var xpos = x * Globals.SQ_SIZE
 			var ypos = y * Globals.SQ_SIZE
--- src/Unit.gd
+++ src/Unit.gd
@@ -5,7 +5,12 @@ const SPEED = 2
 onready var main = get_tree().get_root().get_node("Main")
 
 export var side:int
+var main_target # If seen, go ape!
+var squad = [] # Other friendly units
+
 var route = []
+var current_enemy
+var alive = true
 
 func _ready():
 	pass
@@ -13,6 +18,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if current_enemy != null:
+		if current_enemy.alive == false or is_in_line_of_sight(current_enemy):
+			current_enemy = null
+		else:
+			shoot()
+		return
+		
 	if route.empty():
 		var dest = Vector2(200, 200)
 		route = Globals.get_route(self.position, dest)
@@ -30,8 +42,8 @@ func _on_Timer_CheckForEnemies_timeout():
 	for unit in main.units:
 		if unit.side == self.side:
 			continue
-#		if self.position.distance_to(unit.position) > 100:
-#			continue
+		if self.position.distance_to(unit.position) > 100:
+			continue
 		if is_in_line_of_sight(unit) == false:
 			continue
 		print("Can see")
@@ -49,3 +61,7 @@ func is_in_line_of_sight(thing):
 		return false
 			
 
+func shoot():
+	#todo
+	pass
+	
